cb1f7ca8e25d8adafa5fbda7a84f7f935b60861b
afeiship <1290657123@qq.com>
2022-12-10 16:51:03 +0800

    fix: timeout

--- packages/next-fetch-with-timeout/src/index.js
+++ packages/next-fetch-with-timeout/src/index.js
@@ -1,23 +1,20 @@
 (function () {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@jswork/next');
-  var DEFAULT_OPTIONS = { timeout: 0 };
+  var defaults = { timeout: 0 };
   var TIMEOUT_ERROR = { type: 'timeout', message: 'Timeout: from `next-fetch-with-timeout`' };
 
   nx.fetchWithTimeout = function (inFetch) {
     return function (inUrl, inOptions) {
-      var options = nx.mix(null, DEFAULT_OPTIONS, inOptions);
+      var timer, ctrl;
+      var options = nx.mix(null, defaults, inOptions);
       if (!options.timeout) return inFetch(inUrl, options);
-      var ctrl = new AbortController();
+      ctrl = new AbortController();
       options = nx.mix(null, { signal: ctrl.signal }, inOptions);
-      var timer = setTimeout(function () {
-        ctrl.abort();
-      }, options.timeout);
+      timer = setTimeout(() => ctrl.abort(), options.timeout);
 
       return inFetch(inUrl, options)
-        .finally(function () {
-          clearTimeout(timer);
-        })
+        .finally(() => clearTimeout(timer))
         .catch(function (err) {
           if (err.type === 'aborted') throw TIMEOUT_ERROR;
           throw err;
