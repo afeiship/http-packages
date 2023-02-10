629dc3d9b4e76cf99b65b00c5eb91793c53f2ff1
afeiship <1290657123@qq.com>
2023-02-10 23:26:36 +0800

    feat: refactor to 2.0

--- packages/http-schema/src/index.ts
+++ packages/http-schema/src/index.ts
@@ -4,34 +4,45 @@ import '@jswork/next';
 import '@jswork/next-tmpl';
 import '@jswork/next-data-transform';
 
+const URL_ACTIONS = ['get', 'delete', 'head', 'options'];
+
 export default (inConfig, inInitOptions?: CreateAxiosDefaults): any => {
   const httpClient = axios.create(inInitOptions);
   const context = {};
-  var request = inConfig.request;
-  var items = inConfig.items;
-  var baseUrl = inConfig.baseURL || '//' + location.host;
-  var prefix = inConfig.prefix || '';
+  const request = inConfig.request;
+  const items = inConfig.items;
+  const baseUrl = inConfig.baseURL || '//' + location.host;
+  const prefix = inConfig.prefix || '';
 
   items.forEach(function (item) {
-    var _request = item.request;
-    var _items = item.items;
-    var _prefix = item.prefix || prefix;
-    var _host = item.baseURL;
+    const _request = item.request;
+    const _items = item.items;
+    const _prefix = item.prefix || prefix;
+    const _host = item.baseURL;
 
     nx.each(_items, function (key, _item) {
-      var apiKey = _prefix + key;
+      const apiKey = _prefix + key;
       context[apiKey] = function (inData, inOptions) {
-        var data = Array.isArray(inData) ? nx.mix.apply(nx, inData) : inData;
-        var action = String(_item[0]).toLowerCase();
-        var requestData = _request || request;
-        var context = requestData[0];
-        var dpData = dp(_item[1], data);
-        var apiPath = nx.tmpl(_item[1], dpData[0]);
-        var options = nx.mix(null, _item[2], inOptions);
-        var dataType = nx.get(options, 'dataType', requestData[1]);
-        var params = action === 'get' ? dpData[1] : nx.NxDataTransform[dataType](dpData[1]);
+        const data = Array.isArray(inData) ? nx.mix.apply(nx, inData) : inData;
+        const action = String(_item[0]).toLowerCase();
+        const requestData = _request || request;
+        const context = requestData[0];
+        const dpData = dp(_item[1], data);
+        const apiPath = nx.tmpl(_item[1], dpData[0]);
+        const options = nx.mix(null, _item[2], inOptions);
+        const dataType = nx.get(options, 'dataType', requestData[1]);
+        const isGetStyle = URL_ACTIONS.includes(action);
+        const params = dpData[1];
+        const body = nx.NxDataTransform[dataType](dpData[1]);
 
-        return httpClient[action]((_host || baseUrl) + context + apiPath, params, options);
+        // return httpClient[action]((_host || baseUrl) + context + apiPath, params, options);
+        return httpClient.request({
+          url: (_host || baseUrl) + context + apiPath,
+          method: action,
+          params,
+          data: isGetStyle ? undefined : body,
+          ...options
+        });
       };
     });
   });
