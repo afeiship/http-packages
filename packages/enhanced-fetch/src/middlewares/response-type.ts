99fbfacf030e498579654cd3b6755739a62c8e87
afeiship <1290657123@qq.com>
2023-04-07 19:41:40 +0800

    refactor: upt respnose-type

--- packages/enhanced-fetch/src/middlewares/response-type.ts
+++ packages/enhanced-fetch/src/middlewares/response-type.ts
@@ -4,11 +4,6 @@ const defaults = { responseType: 'json' };
 
 export const middlewareResponseType: MiddleWareFunction = (inFetch) => (inUrl, inInit?) => {
   const { responseType, ...options } = { ...defaults, ...inInit };
-  return inFetch(inUrl, options).then((response) => {
-    if (responseType) {
-      return response[responseType]();
-    } else {
-      return response;
-    }
-  });
+  if (!responseType) return inFetch(inUrl, options);
+  return inFetch(inUrl, options).then((response) => response[responseType]());
 };
