f5e3012c2a10fa5f9317084668f1c07e0e0e9e8a
afeiship <1290657123@qq.com>
2023-04-07 20:08:38 +0800

    fix: typing EnhancedRequestInit

--- packages/enhanced-fetch/src/types.ts
+++ packages/enhanced-fetch/src/types.ts
@@ -1,13 +1,13 @@
 export type ResponseType = 'json' | 'text' | 'blob' | 'arrayBuffer' | null;
 export type DestroyablePromise<T> = { destroy?: () => void } & Promise<T>;
-export type FetchFn = (
-  input: RequestInfo | URL,
-  init?: RequestInit
-) => DestroyablePromise<Response>;
-export type MiddleWareFunction = (inFetch: FetchFn) => FetchFn;
 export interface EnhancedRequestInit extends RequestInit {
   timeout?: number;
   destroyable?: boolean;
   debug?: boolean;
   responseType?: ResponseType;
 }
+export type FetchFn = (
+  input: RequestInfo | URL,
+  init?: EnhancedRequestInit
+) => DestroyablePromise<Response>;
+export type MiddleWareFunction = (inFetch: FetchFn) => FetchFn;
