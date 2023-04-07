12f78581f3b7bdd98d8ac5a38a31708f0cf49e0f
afeiship <1290657123@qq.com>
2023-04-07 13:34:26 +0800

    fix: new impl

--- packages/next-apply-fetch-middleware/index.d.ts
+++ packages/next-apply-fetch-middleware/index.d.ts
@@ -2,7 +2,5 @@ type FetchFn = (inUrl: string, inOptions: RequestInit) => Response;
 type Middleware = (inFetch: FetchFn) => FetchFn;
 
 interface NxStatic {
-  applyFetchMiddleware: (
-    middlewares: Middleware[]
-  ) => (inUrl: string, inOptions: RequestInit) => Response;
+  applyFetchMiddleware: (middlewares: Middleware[]) => FetchFn;
 }
