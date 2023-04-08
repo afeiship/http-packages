d909e5aa611115f11f93a5a9091b112201e3e962
afeiship <1290657123@qq.com>
2023-04-08 08:34:02 +0800

    refactor: new impl

--- packages/fetch/src/index.ts
+++ packages/fetch/src/index.ts
@@ -1,28 +1,24 @@
-const globalObject =
-  typeof window !== 'undefined'
-    ? window
-    : typeof globalThis !== 'undefined'
-    ? globalThis
-    : typeof global !== 'undefined'
-    ? global
-    : typeof self !== 'undefined'
-    ? self
-    : {};
+declare global {
+  namespace NodeJS {
+    interface Global {
+      fetch: (input: RequestInfo, init?: RequestInit) => Promise<Response>;
+      FormData: typeof FormData;
+    }
+  }
+}
 
 let stdFetch;
 
-if (typeof fetch !== 'undefined') stdFetch = fetch;
-
-try {
+// nodejs
+if (typeof process !== 'undefined' && process?.versions?.node) {
   stdFetch = require('node-fetch').default;
   const FormData = require('form-data');
-  globalObject['fetch'] = stdFetch;
-  globalObject['FormData'] = FormData;
-} catch (error) {
-  console.error('To use fetch in Node.js environment, please install "node-fetch" package:');
-  console.error('yarn add node-fetch@2.6.7');
-  process.exit(1);
+  global.fetch = stdFetch;
+  global.FormData = FormData;
 }
+
+// other with fetch env:
+if (typeof fetch !== 'undefined') stdFetch = fetch;
 
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = stdFetch;
