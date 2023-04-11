9adf21b5f55ed1f37ca20ead3825d8ebbedd5282
afeiship <1290657123@qq.com>
2023-04-11 22:48:19 +0800

    fix: web/node the same

--- packages/web-http-schema/src/index.ts
+++ packages/web-http-schema/src/index.ts
@@ -1,24 +1,8 @@
 import nx from '@jswork/next';
-import NxFetch from '@jswork/next-fetch';
 import httpRestConfig from '@jswork/http-rest-config';
-import nxFetchWithDebug from '@jswork/next-fetch-with-debug';
-import nxFetchWithTimeout from '@jswork/next-fetch-with-timeout';
-import nxFetchWithCancelable from '@jswork/next-fetch-with-cancelable';
-import nxFetchWithResponseType from '@jswork/next-fetch-with-response-type';
-import '@jswork/next-apply-fetch-middleware';
+import '@jswork/next-fetch';
 
 export default (inConfig, inOptions?) => {
-  const fetch = nx.applyFetchMiddleware([
-    nxFetchWithTimeout,
-    nxFetchWithCancelable,
-    nxFetchWithResponseType,
-    nxFetchWithDebug
-  ])(global.fetch);
-
-  const http = NxFetch.getInstance({
-    fetch,
-    ...inOptions
-  });
-
+  const http = nx.Fetch.getInstance(inOptions);
   return httpRestConfig(http, inConfig);
 };
