9e52dcc074d9c68f5eaffd9f7de535121c23fe4e
afeiship <1290657123@qq.com>
2023-04-11 22:38:29 +0800

    feat: basic case

--- packages/node-http-schema/__tests__/index.spec.ts
+++ packages/node-http-schema/__tests__/index.spec.ts
@@ -1,7 +1,20 @@
 import fn from '../src';
 
 describe('api.basic', () => {
-  test('normail single value case', () => {
-    expect(fn({ rel: true })).toBe(undefined);
+  test('schma should be $api', async () => {
+    const $api = fn({
+      baseURL: 'https://api.github.com',
+      request: ['', 'json'],
+      items: [
+        {
+          items: {
+            login: ['get', '/users/afeiship'],
+          },
+        },
+      ],
+    }) as any;
+
+    const { data } = await $api.login();
+    expect(data.login).toBe('afeiship');
   });
 });
