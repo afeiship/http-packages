33567fb7fe5d163c34655ab2936449c22f64d830
afeiship <1290657123@qq.com>
2023-05-19 21:43:19 +0800

    feat: add minaFramework

--- packages/normalize-mina-options/src/index.ts
+++ packages/normalize-mina-options/src/index.ts
@@ -1,10 +1,26 @@
-const NormalizeMinaOptions = (): void => {
-  console.log('hello');
+export type MinaOptions = {
+  minaFramework?: 'mina' | 'uniapp' | 'taro';
+} & Record<string, any>;
+
+const normalizeMinaOptions = (inOptions: MinaOptions) => {
+  const { minaFramework, ...options } = inOptions;
+  const headers = options.headers;
+  const responseType = options.responseType;
+
+  headers['content-type'] = headers['Content-Type'];
+  options.data = options.body;
+  options.header = headers;
+  options.responseType = responseType === 'json' ? 'text' : responseType;
+  delete headers['Content-Type'];
+  delete options.headers;
+  delete options.body;
+
+  return options;
 };
 
 // for commonjs es5 require
 if (typeof module !== 'undefined' && module.exports) {
-  module.exports = NormalizeMinaOptions;
+  module.exports = normalizeMinaOptions;
 }
 
-export default NormalizeMinaOptions;
+export default normalizeMinaOptions;
