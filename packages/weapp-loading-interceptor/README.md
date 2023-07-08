70f54da5512fa00dd4e77d52bd892099530ec4af
afeiship <1290657123@qq.com>
2023-07-08 18:11:37 +0800

    feat: init loading interceptor

---
+++ packages/weapp-loading-interceptor/README.md
@@ -0,0 +1,34 @@
+# weapp-loading-interceptor
+> Loading interceptor for weapp.
+
+[![version][version-image]][version-url]
+[![license][license-image]][license-url]
+[![size][size-image]][size-url]
+[![download][download-image]][download-url]
+
+## installation
+```shell
+npm install @jswork/weapp-loading-interceptor
+```
+
+## usage
+```js
+import weappLoadingInterceptor from '@jswork/weapp-loading-interceptor';
+
+// usage goes here.
+```
+
+## license
+Code released under [the MIT license](https://github.com/afeiship/weapp-loading-interceptor/blob/master/LICENSE.txt).
+
+[version-image]: https://img.shields.io/npm/v/@jswork/weapp-loading-interceptor
+[version-url]: https://npmjs.org/package/@jswork/weapp-loading-interceptor
+
+[license-image]: https://img.shields.io/npm/l/@jswork/weapp-loading-interceptor
+[license-url]: https://github.com/afeiship/weapp-loading-interceptor/blob/master/LICENSE.txt
+
+[size-image]: https://img.shields.io/bundlephobia/minzip/@jswork/weapp-loading-interceptor
+[size-url]: https://github.com/afeiship/weapp-loading-interceptor/blob/master/dist/weapp-loading-interceptor.min.js
+
+[download-image]: https://img.shields.io/npm/dm/@jswork/weapp-loading-interceptor
+[download-url]: https://www.npmjs.com/package/@jswork/weapp-loading-interceptor
---
+++ packages/weapp-loading-interceptor/src/index.ts
@@ -0,0 +1,66 @@
+declare var wx: any;
+
+interface Options {
+  interval?: number;
+  ignoreKey?: string;
+  onShow?: (opts: any) => void;
+  onHide?: (opts: any) => void;
+}
+
+const defaults: Options = {
+  interval: 500,
+  ignoreKey: 'ignoreLoading',
+  onShow: (opts) => {
+    wx.showLoading({ title: '加载中' });
+  },
+  onHide: (opts) => {
+    wx.hideLoading();
+  },
+};
+
+class WhenLoading {
+  private options: Options;
+  private requestCount = 0;
+  private loadingTimer;
+
+  constructor(inOptions: Options) {
+    this.options = Object.assign({}, defaults, inOptions);
+  }
+
+  create() {
+    return [
+      { type: 'request', fn: (opts) => this.whenRequest(opts) },
+      { type: 'response', fn: (opts) => this.whenResponse(opts) },
+    ];
+  }
+
+  whenRequest(inOptions) {
+    const { onShow, ignoreKey } = this.options;
+    const isIgnore = inOptions[ignoreKey!];
+    this.loadingTimer && clearTimeout(this.loadingTimer);
+    if (this.requestCount === 0 && !isIgnore) onShow!(inOptions);
+    this.requestCount++;
+    return inOptions;
+  }
+
+  whenResponse(inOptions) {
+    const { onHide, interval } = this.options;
+    this.requestCount--;
+    this.loadingTimer = setTimeout(() => {
+      if (this.requestCount === 0) onHide!(inOptions);
+    }, interval);
+    return inOptions;
+  }
+}
+
+const createInterceptors = (options) => {
+  const instance = new WhenLoading(options);
+  return instance.create();
+};
+
+// for commonjs es5 require
+if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
+  module.exports = createInterceptors;
+}
+
+export default createInterceptors;
