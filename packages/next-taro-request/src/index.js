d825a777412892eb8164b3c18979d902534aadb6
afeiship <1290657123@qq.com>
2024-01-18 11:09:30 +0800

    refactor: taro request

--- packages/next-taro-request/src/index.js
+++ packages/next-taro-request/src/index.js
@@ -1,51 +1,41 @@
-(function () {
-  var global = typeof window !== 'undefined' ? window : this || Function('return this')();
-  var nx = global.nx || require('@jswork/next');
-  var defaults = { responseType: 'text' };
-  var Taro = global.Taro || require('@tarojs/taro');
-  var _ = require('@jswork/next-json');
+import nx from '@jswork/next';
+import '@jswork/next-abstract-request';
+import normalizeMinaOptions from '@jswork/normalize-mina-options';
 
-  var normalize = function (inOptions) {
-    var headers = inOptions.headers;
-    var responseType = inOptions.responseType;
-    headers['content-type'] = headers['Content-Type'];
-    inOptions.data = inOptions.body;
-    inOptions.header = headers;
-    inOptions.responseType = responseType === 'json' ? 'text' : responseType;
-    delete headers['Content-Type'];
-    delete inOptions.headers;
-    delete inOptions.body;
-    return inOptions;
-  };
+const NxTaroRequest = nx.declare('nx.TaroRequest', {
+  extends: nx.AbstractRequest,
+  methods: {
+    initClient: function() {
+      this.httpRequest = function(inOptions) {
+        const opts = normalizeMinaOptions({ minaFramework: 'taro', ...inOptions });
+        const { url: _url, params: _params, $url, $query, $body } = opts;
+        const params = $query || _params;
+        const url = $url || nx.param(params, _url);
+        const data = $body || opts.data;
 
-  nx.taroRequest = function (inUrl, inOptions) {
-    var options = nx.mix(null, { url: inUrl }, defaults, inOptions);
-    options = normalize(options);
-    return new Promise(function (resolve, reject) {
-      Taro.request(
-        nx.mix(
-          {
-            success: function (res) {
-              var responseType = options.responseType;
-              // 网络正常，但服务器异常返回，如返回一段xml/html
-              res.data = responseType === 'text' ? nx.parse(res.data) : res.data;
-              if (res.statusCode !== 200) return reject(res);
-              resolve(res);
-            },
-            // {errMsg: "request:fail "}
-            // 这种怦下说明网络有问题；nginx断了，网络断了等情况
-            fail: function (err) {
-              err.statusCode = -1;
-              reject(err);
-            }
-          },
-          options
-        )
-      );
-    });
-  };
-
-  if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
-    module.exports = nx.taroRequest;
+        return Taro
+          .request({ ...opts, data, url })
+          .then((res) => {
+            return {
+              status: res.statusCode,
+              data: res.data,
+              original: res
+            };
+          })
+          .catch((err) => {
+            return Promise.reject({
+              status: err.statusCode,
+              data: err.data,
+              original: err
+            });
+          });
+      };
+    }
   }
-})();
+});
+
+if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
+  module.exports = TaroRequest;
+}
+
+export default TaroRequest;
