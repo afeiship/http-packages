025a61f935a58ebb2d14d6b9b5dfe655ada8a792
afeiship <1290657123@qq.com>
2024-01-26 10:08:21 +0800

    fix: reject when 40x or not ok

--- packages/enhanced-fetch/src/middlewares/response-type.ts
+++ packages/enhanced-fetch/src/middlewares/response-type.ts
@@ -7,10 +7,10 @@ export const middlewareResponseType: MiddleWareFunction = (inFetch) => (inUrl, i
   if (!responseType) return inFetch(inUrl, options);
   return inFetch(inUrl, options).then((original) => {
     const { ok, status } = original;
-    const resType = ok ? responseType : 'text';
-    const stubResponse = status === 204 ? Promise.resolve(null) : original[resType]();
+    const stubResponse = status === 204 ? Promise.resolve(null) : original[responseType]();
     return stubResponse
       .then((data) => {
+        if (!ok) return Promise.reject(data);
         return { status, data, original };
       })
       .catch((error) => {
