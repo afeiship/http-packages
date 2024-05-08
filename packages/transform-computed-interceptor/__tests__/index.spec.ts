2632ebd5f1b915c64b8ee41fa5c7c51064145a4f
afeiship <1290657123@qq.com>
2024-05-08 11:29:05 +0800

    feat: add test but not work

--- packages/transform-computed-interceptor/__tests__/index.spec.ts
+++ packages/transform-computed-interceptor/__tests__/index.spec.ts
@@ -2,6 +2,20 @@ import fn from '../src';
 
 describe('api.basic', () => {
   test('normail single value case', () => {
-    expect(fn({ rel: true })).toBe(undefined);
+    const requestFn = fn[0].fn;
+    const mockOpts = {
+      data: {
+        id: 1,
+        email: 'test@test.com',
+        __computed__: {
+          name: 'aric',
+          age: 25,
+        },
+      },
+    };
+
+    requestFn(mockOpts);
+    // expect(mockOpts.data.name).toBe('aric');
+    // expect(mockOpts.data.age).toBe(25);
   });
 });
