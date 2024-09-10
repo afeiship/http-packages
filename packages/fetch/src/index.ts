25b3bd3f1c3414f9c19584ec8245e8dc1fe471bf
afeiship <1290657123@qq.com>
2024-09-10 21:42:11 +0800

    fix: rm node-fetch deps

--- packages/fetch/src/index.ts
+++ packages/fetch/src/index.ts
@@ -9,13 +9,13 @@ declare global {
 
 let stdFetch;
 
-// nodejs
-if (typeof process !== 'undefined' && process?.versions?.node) {
-  stdFetch = require('node-fetch').default;
-  const FormData = require('form-data');
-  global.fetch = stdFetch;
-  global.FormData = FormData;
-}
+// nodejs 18+ has built-in fetch
+// if (typeof process !== 'undefined' && process?.versions?.node) {
+//   stdFetch = require('node-fetch').default;
+//   const FormData = require('form-data');
+//   global.fetch = stdFetch;
+//   global.FormData = FormData;
+// }
 
 // other with fetch env:
 if (typeof fetch !== 'undefined') stdFetch = fetch;
